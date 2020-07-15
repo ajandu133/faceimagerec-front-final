0e7de756dc3371870b919c399b4d0acd3f8aefb1
ajandu133 <67841621+ajandu133@users.noreply.github.com>
2020-07-15 12:32:01 -0400

    update

--- src/App.js
+++ src/App.js
@@ -36,6 +36,20 @@ const particlesOptions = {
         }
       }
 
+const initialState = {
+  input: '',
+  imageUrl:'',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+}}
+
 class App extends Component {
   constructor() {
     super();
@@ -117,7 +131,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
